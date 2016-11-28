e9fa47ecdd834085c6700ba7a09776ff6015358a
Edele Gizatullin <me@edele.ru>
2016-11-28 23:56:34 +0500

    test players backend

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::group(['middleware' => 'cors'], function () {
+
+    Route::resource('players', 'PlayerController');
+
+});
