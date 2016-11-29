5b97e3d686428da2ec9d84ded5031a720e778a94
Edele Gizatullin <me@edele.ru>
2016-11-29 21:55:11 +0500

    fix cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,8 +7,10 @@ use Closure;
 class Cors {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $content = $next($request);
+        return ( new Response($content) )->header('Access-Control-Allow-Origin' , '*')
+                                           ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                                           ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
+         }
     }
 }
